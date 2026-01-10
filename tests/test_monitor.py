83a0e910c1afabfb2f5a5d321ba53a1f38982c4c
Emir Risyad <emirrsyd@gmail.com>
2026-01-10 23:04:09 +0100

    test: add test for connection error

--- tests/test_monitor.py
+++ tests/test_monitor.py
@@ -1,5 +1,8 @@
 import pytest
 from datetime import timedelta
+
+import requests
+
 from producer import get_website_status
 from unittest.mock import patch, Mock
 
@@ -20,3 +23,13 @@ def test_website_status_logic(mock_get, url, status_code, latency):
     assert result['url'] == url
     assert result['status_code'] == status_code
     assert result['latency'] == latency
+
+@patch('requests.get')
+def test_website_connection_error(mock_get):
+    mock_get.side_effect = requests.exceptions.ConnectionError("ConnectionError")
+
+    result = get_website_status("https://broken-link.com")
+
+    assert result['url'] == "https://broken-link.com"
+    assert result['status_code'] == 0
+    assert "ConnectionError" in result['error']
