cce7832b691d018734acc996470f0773d8c46e77
Emir Risyad <emirrsyd@gmail.com>
2026-01-10 21:13:14 +0100

    feat: implement real-time Discord notifications for service failures

--- consumer.py
+++ consumer.py
@@ -1,3 +1,5 @@
+import requests
+from datetime import datetime
 from confluent_kafka import Consumer
 import os
 import json
@@ -5,28 +7,61 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-conf = {'bootstrap.servers':os.getenv('KAFKA_BOOTSTRAP_SERVERS'),
-        'group.id': os.getenv('KAFKA_GROUP_ID'),
-        'auto.offset.reset': 'earliest',
-        }
+def send_discord_alert(data):
+    readable_time = datetime.fromtimestamp(data['time']).strftime('%d-%m-%Y %H:%M:%S')
+    embed_content = {
+        "title": "🔥 WEBSITE DOWN ALERT 🔥",
+        "description": f"The monitor detected a failure.",
+        "color": 15158332,
+        "fields": [
+            {"name": "Target URL", "value": data['url'], "inline": False},
+            {"name": "Status Code", "value": data['status'], "inline": False},
+            #{"name": "Error Message", "value": data['error_message', 'None'], "inline": True},
+            {"name": "Timestamp", "value": readable_time, "inline": False},
+        ]
+    }
 
-consumer = Consumer(conf)
-consumer.subscribe([os.getenv('KAFKA_TOPIC_NAME')])
+    payload = {
+        "username": "Uptime Bot",
+        "embeds": [embed_content]
+    }
 
-try:
-    while True:
-        msg = consumer.poll(1.0)
-
-        if msg is None: continue
-        if msg.error():
-            print("Consumer error: {}".format(msg.error()))
-            continue
-
-        data = json.loads(msg.value().decode('utf-8'))
-        if data['status'] >= 400:
-            print(f"CRITICAL: {data['url']} is DOWN with status {data['status']}!")
+    try:
+        response = requests.post(os.getenv("DISCORD_WEBHOOK_URL"), json=payload)
+        if response.status_code == 204:
+            print("Discord alert sent successfully")
         else:
-            print(f"OK: {data['url']} is UP with status {data['status']}!")
+            print(f"Failed to send Discord alert: {response.status_code} - {response.text}")
+    except Exception as e:
+        print(f"Error sending alert: {e}")
 
-finally:
-    consumer.close()
+def main():
+    conf = {'bootstrap.servers':os.getenv('KAFKA_BOOTSTRAP_SERVERS'),
+            'group.id': os.getenv('KAFKA_GROUP_ID'),
+            'auto.offset.reset': 'latest',
+            }
+
+    consumer = Consumer(conf)
+    consumer.subscribe([os.getenv('KAFKA_TOPIC_NAME')])
+
+    try:
+        while True:
+            msg = consumer.poll(1.0)
+
+            if msg is None: continue
+            if msg.error():
+                print("Consumer error: {}".format(msg.error()))
+                continue
+
+            data = json.loads(msg.value().decode('utf-8'))
+            if data['status'] != 200:
+                print(f"CRITICAL: {data['url']} is DOWN with status {data['status']}!")
+                send_discord_alert(data)
+            else:
+                print(f"OK: {data['url']} is UP with status {data['status']}!")
+
+    finally:
+        consumer.close()
+
+if __name__ == "__main__":
+    main()
