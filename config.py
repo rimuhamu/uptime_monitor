e99b87f6645513e98f25c26b562005f4b676d4f3
Emir Risyad <emirrsyd@gmail.com>
2026-01-10 18:06:22 +0100

    feat: initial setup

---
+++ config.py
@@ -0,0 +1,4 @@
+BOOTSTRAP_SERVERS = ['localhost:9092']
+TOPIC_NAME = 'website_health'
+SITES_TO_MONITOR = ['https://www.emirrisyad.site/', 'https://www.shinjang.site/', 'https://httpstat.us/404'] # Last one tests errors
+SLEEP_TIME = 10
