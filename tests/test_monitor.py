2fcd699dadf3b31a8dafb2d80827d9d16fc2594f
Emir Risyad <emirrsyd@gmail.com>
2026-01-10 22:55:26 +0100

    test: add unit test for website status

---
+++ tests/test_monitor.py
@@ -0,0 +1,22 @@
+import pytest
+from datetime import timedelta
+from producer import get_website_status
+from unittest.mock import patch, Mock
+
+@patch('requests.get')
+@pytest.mark.parametrize("url, status_code, latency",[
+    ("https://google.com", 200, 0.5),
+    ("https://httpstat.us/404", 404, 0.1),
+    ("https://mysite.com/down", 503, 0.0)
+])
+def test_website_status_logic(mock_get, url, status_code, latency):
+    mock_response = Mock()
+    mock_response.status_code = status_code
+    mock_response.elapsed= timedelta(seconds=latency)
+    mock_get.return_value = mock_response
+
+    result = get_website_status(url)
+
+    assert result['url'] == url
+    assert result['status_code'] == status_code
+    assert result['latency'] == latency
